ae394fc51d94d20572cdfa4baf262ed56587d708
Hademola <manifirsttech@gmail.com>
2026-08-28 00:59:42 +0100

    Update Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -481,7 +481,7 @@ function Home() {
             </div>
           </div>
           <div className="footer-bottom">
-            <p>&copy; 2024 Layo Henna Touch. All rights reserved.</p>
+            <p>&copy; 2026 Layo Henna Touch. All rights reserved.</p>
           </div>
         </div>
       </footer>
